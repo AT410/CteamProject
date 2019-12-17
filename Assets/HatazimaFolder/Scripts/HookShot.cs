dc58cb92ccb71b6f502a4e4d47c2f8e0c143f2ca
Hatazima <nkc19.zyou.hatazima@gmail.com>
2019-12-17 12:26:46 +0900

    縄で壁に移動する機能を自動から手動に変更

--- Assets/HatazimaFolder/Scripts/HookShot.cs
+++ Assets/HatazimaFolder/Scripts/HookShot.cs
@@ -4,31 +4,57 @@ using UnityEngine;
 
 public class HookShot : MonoBehaviour
 {
-    Vector3 targ;           //レイが当たったオブジェクト
+    Vector3 targ;           //レイが当たったオブジェクトの座標
     RaycastHit hit;         //レイが当たったオブジェクトの様々な情報を格納する変数
     float dist;             //プレイヤーとオブジェクトの距離を格納する変数
     float time = 0;         //プレイヤーが移動する時間を測る変数
-    float moveTime = 0.85f; //プレイヤーが移動する時間
+    float moveTime = 0.8f; //プレイヤーが移動する時間
     bool move = false;      //プレイヤーが移動しているか判断する変数
-    
+
+    public GameObject rope;
+    LineRenderer line;
+
+    void Start()
+    {
+        //LineRendererコンポーネントの追加
+        line = rope.AddComponent<LineRenderer>();
+        //加算合成をマテリアルに適用
+        line.material = new Material(Shader.Find("Mobile/Particles/Additive"));
+        //線の頂点数
+        line.positionCount = 2;
+        //線の太さ
+        line.startWidth = 0.1f;
+        line.endWidth = 0.1f;
+        //線の色
+        line.startColor = new Color(255, 180, 0, 255);
+        line.endColor = new Color(255, 180, 0, 255);
+        rope.SetActive(false);
+    }
+
     void Update()
     {
-        if (!move)
-        {
-            if (Input.GetKeyDown(KeyCode.Return)) ObjectDesignation();
-        }
-        if (move)
+        if (Input.GetMouseButtonDown(0)) ObjectDesignation();
+        //if (!move)
+        //{
+        //    if (Input.GetMouseButtonDown(0)) ObjectDesignation();
+        //}
+        if (move && Input.mouseScrollDelta.y != 0)
         {
             time += Time.deltaTime;
             
-            if(time >= moveTime) //moveTimeの時間だけ移動したら止まる
+            //指定した座標まで移動させる
+            transform.position = Vector3.MoveTowards(transform.position, targ, dist * Time.deltaTime);
+            
+            //線の座標指定
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, targ);
+
+            if (time >= dist * 0.12f) //moveTimeの時間だけ移動したら止まる
             {
+                rope.SetActive(false);
                 move = false;
                 time = 0;
             }
-
-            //指定した座標まで移動させる
-            transform.position = Vector3.MoveTowards(transform.position, targ, dist * Time.deltaTime);
         }
 
         //テスト用
@@ -52,7 +78,13 @@ public class HookShot : MonoBehaviour
         {
             targ = hit.transform.position;
             dist = Vector3.Distance(transform.position, targ); //現在位置と選択したオブジェクトまでの距離を測る
+            Debug.Log(dist);
             move = true;
+            rope.SetActive(true);
+            //線の座標指定
+            line.SetPosition(0, transform.position);
+            line.SetPosition(1, targ);
+            time = 0;
         }
     }
 }
