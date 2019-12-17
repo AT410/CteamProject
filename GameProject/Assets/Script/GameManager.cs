762f5a77987d47f75296f7cf03194b4b1ba0cec0
達哉 阿部 <zeal18300092@c-league.jp>
2019-12-17 12:54:53 +0900

    [new]Editor拡張（パラメーターデータ作成）

--- GameProject/Assets/Script/GameManager.cs
+++ GameProject/Assets/Script/GameManager.cs
@@ -4,11 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    /// <summary>
+    /// オブジェクトデータ
+    /// </summary>
+    [SerializeField]
+    private List<ObjectData> Datas = new List<ObjectData>();
+
+    private List<GameObject> Objects;
+    /// <summary>
+    /// 同時に生成されるオブジェクト上限数
+    /// </summary>
+    [SerializeField]
+    [Range(1,50)]
+    private int MaxNumObjects;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Objects = new List<GameObject>();
+        Debug.Log(Objects.Count);
     }
 
     // Update is called once per frame
@@ -20,4 +34,6 @@ public class GameManager : MonoBehaviour
     //生成関数
 
     //死亡管理
+
+
 }
---
+++ GameProject/Assets/Script/ObjectData.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEditor;
+
+public class ObjectData : ScriptableObject
+{
+    /// <summary>
+    /// オブジェクト用パラメータデータ
+    /// </summary>
+    public ObjectType type;
+
+    //時間制限で消えるかどうか
+    [SerializeField]
+    protected bool IsLimitActive=false;
+
+    //セッター・ゲッター
+    public bool GetDelFlag
+    {
+        get
+        {
+            return IsLimitActive;
+        }
+#if UNITY_EDITOR
+        set
+        {
+            IsLimitActive = value;
+        }
+#endif
+    }
+}
