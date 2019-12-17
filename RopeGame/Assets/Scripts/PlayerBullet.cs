761e9dc309b53f884df2345174941007af6d90a2
DavidDuarte1993 <49542308+DavidDuarte1993@users.noreply.github.com>
2019-12-17 15:03:34 +0900

    RopeGame

---
+++ RopeGame/Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public static PlayerBullet instance;
+    public float speed = 7.5f;
+    public Rigidbody2D theRB;
+    public Transform bulletTrans;
+
+    public GameObject impactEffect;
+
+    public int damageToGive = 50;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = transform.right * speed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Instantiate(impactEffect, transform.position, transform.rotation);
+        PlayerController.instance.playerTrans.position = PlayerBullet.instance.bulletTrans.position;
+
+        Destroy(gameObject);
+
+        if(other.tag == "Enemy")
+        {
+            other.GetComponent<EnemyController>().DamageEnemy(damageToGive);
+        }
+    }
+
+    private void OnBecomeInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ RopeGame/Assets/Scripts/PlayerHealthController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthController : MonoBehaviour
+{
+    public static PlayerHealthController instance;
+
+    public int currentHealth;
+    public int maxHealth;
+
+    public float damageInvincLength = 1f;
+    private float invictCount;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UIController.instance.healthSlider.maxValue = maxHealth;
+        UIController.instance.healthSlider.value = currentHealth;
+        UIController.instance.healthText.text = currentHealth.ToString() + " / " + maxHealth.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(invictCount > 0)
+        {
+            invictCount -= Time.deltaTime;
+
+            if(invictCount <= 0)
+            {
+                PlayerController.instance.bodySR.color = new Color(PlayerController.instance.bodySR.color.r, PlayerController.instance.bodySR.color.g, PlayerController.instance.bodySR.color.b, 1f);
+            }
+        }
+    }
+
+    public void DamagePlayer()
+    {
+        if(invictCount <= 0)
+        {
+           currentHealth--;
+
+            invictCount = damageInvincLength;
+
+            PlayerController.instance.bodySR.color = new Color(PlayerController.instance.bodySR.color.r, PlayerController.instance.bodySR.color.g, PlayerController.instance.bodySR.color.b, .5f);
+        
+           if(currentHealth <= 0)
+           {
+               PlayerController.instance.gameObject.SetActive(false);
+
+               UIController.instance.deathScreen.SetActive(true);
+           }
+
+           UIController.instance.healthSlider.value = currentHealth;
+           UIController.instance.healthText.text = currentHealth.ToString() + " / " + maxHealth.ToString();
+        }
+    }
+}
