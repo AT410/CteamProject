c0a7a7fb9d93571f0384fa0ed46f64a78e1fea75
Hatazima <nkc19.zyou.hatazima@gmail.com>
2019-12-18 14:08:24 +0900

    敵が消えるように変更

---
+++ RopeGame/Assets/HatazimaFolder/Scripts/Test.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    static bool a = false;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    public void StateChange()
+    {
+
+    }
+}
