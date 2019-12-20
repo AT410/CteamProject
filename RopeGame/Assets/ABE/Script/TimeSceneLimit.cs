f1daa105944d225dee37316b12754dd4c17b6ba1
達哉 阿部 <zeal18300092@c-league.jp>
2019-12-20 09:37:21 +0900

    [update]一定時間経過でシーン遷移

---
+++ RopeGame/Assets/ABE/Script/TimeSceneLimit.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimeSceneLimit : MonoBehaviour
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private float _ChangeTime;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField]
+    private string LoadScene;
+
+    private float _currnttime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currnttime = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_currnttime>_ChangeTime)
+            SceneManager.LoadScene(LoadScene);
+        _currnttime += Time.deltaTime;
+    }
+}
